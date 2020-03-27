98e54076cc9664d01030f0b261a32277352b924e
2568808909 <2568808909@qq.com>
2020-03-27 19:35:17 +0800

    learning netty

---
+++ src/main/java/com/ccb/netty/channel/InputFileChannel.java
@@ -0,0 +1,20 @@
+package com.ccb.netty.channel;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class InputFileChannel {
+    public static void main(String[] args) throws IOException {
+        FileInputStream inputStream = new FileInputStream("FileChannelTest.txt");
+        FileChannel channel = inputStream.getChannel();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        channel.read(byteBuffer);
+        byteBuffer.flip();
+        byte[] bytes = new byte[byteBuffer.limit()];
+        byteBuffer.get(bytes);
+        System.out.println(new String(bytes));
+        channel.close();
+    }
+}
---
+++ src/main/java/com/ccb/netty/channel/OutputFileChannel.java
@@ -0,0 +1,25 @@
+package com.ccb.netty.channel;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class OutputFileChannel {
+
+    public static void main(String[] args) throws IOException {
+        String string = "我只是要测试一下FileChannel";
+        FileOutputStream outputStream = new FileOutputStream("FileChannelTest.txt");
+        //通过FileOutputStream获取FileChannel
+        FileChannel channel = outputStream.getChannel();
+        //给ByteBuffer分配1024字节的空间
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        //将字符串放入ByteBuffer中
+        byteBuffer.put(string.getBytes());
+        //读取ByteBuffer前反转一下 limit=position,position=0
+        byteBuffer.flip();
+        //将数据通过FileChannel写出到文件
+        channel.write(byteBuffer);
+        channel.close();
+    }
+}
